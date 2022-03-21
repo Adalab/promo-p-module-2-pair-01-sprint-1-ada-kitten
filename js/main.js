9b2e2c5b569ae486a13a90cac79a218286dbf273
Gloria Rodríguez Fernández <gloriarodrife@gmail.com>
2022-03-21 11:35:47 +0100

    using slice method to trim URL

--- js/main.js
+++ js/main.js
@@ -49,26 +49,31 @@ const card3 = `<li class="card">
 // list.innerHTML = cardList;
 
 // Interpolando variables
-const kittenImage1 = 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg';
+let kittenImage1 = 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg';
+// Metodo con el cual eliminamos el https:// según su posición
+let kittenImageOne = kittenImage1.slice(8);
+console.log(kittenImage1);
+console.log(kittenImageOne);
+
 let kittenName1 = 'Anastacio';
 // Utilizamos el metodo string toUpperCase para modificar el nombre y convertirlo en mayuscula
-kittenName1 = kittenName1.toUpperCase();
-console.log(kittenName1);
-
+const kittenNameOne = kittenName1.toUpperCase();
 const kittenDesc1 =
   'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
 const kittenRace1 = 'British Shorthair';
 
-const kittenImage2 =
+let kittenImage2 =
   'https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg';
+let kittenImageTwo = kittenImage2.slice(8);
 let kittenName2 = 'Fiona';
 kittenName2 = kittenName2.toUpperCase();
 const kittenDesc2 =
   'Ruiseño, juguetón, le gusta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
 const kittenRace2 = 'British Shorthair';
 
-const kittenImage3 =
+let kittenImage3 =
   'https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg';
+let kittenImageThree = kittenImage3.slice(8);
 let kittenName3 = 'Cielo';
 kittenName3 = kittenName3.toUpperCase();
 const kittenDesc3 =
@@ -77,7 +82,7 @@ const kittenRace3 = 'British Shorthair';
 
 const cardKitten1 = `<li class="card">
 <article>
-  <img class="card_img" src=${kittenImage1} alt="gatito" />
+  <img class="card_img" src=" https://${kittenImageOne}" alt="gatito" />
   <h3 class="card_title">${kittenName1}</h3>
   <h4 class="card_race">${kittenRace1}</h4>
   <p class="card_description"> ${kittenDesc1} </p>
@@ -85,18 +90,19 @@ const cardKitten1 = `<li class="card">
 </li>`;
 
 const cardKitten2 = `<li class="card">
-<img class="card_img"src=${kittenImage2} alt="gatito" />
+<img class="card_img"src= " https://${kittenImageTwo}"alt="gatito" />
 <h3 class="card_title">${kittenName2}</h3>
 <h4 class="card_race">${kittenRace2}</h4>
 <p class="card_description" > ${kittenDesc2} </p>
 </li>`;
 
 const cardKitten3 = `<li class="card">
-<img class="card_img" src=${kittenImage3}  alt="gatito" />
+<img class="card_img" src= "https://${kittenImageThree}"  alt="gatito" />
 <h3 class="card_title">${kittenName3}</h3>
 <h4 class="card_race">${kittenRace3}</h4>
 <p class="card_description" > ${kittenDesc3} </p>
 </li> `;
 
+// Pintamos en el HTML el contenido de las variables
 const cardList2 = cardKitten1 + cardKitten2 + cardKitten3;
 list.innerHTML = cardList2;
